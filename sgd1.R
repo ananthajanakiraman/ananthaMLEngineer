30889d46f70f27b0a2aac881d7090107e5938060
ananthajanakiraman <30395005+ananthajanakiraman@users.noreply.github.com>
2018-02-10 12:00:47 -0600

    commit
    commit

--- sgd1.R
+++ sgd1.R
@@ -38,12 +38,12 @@ wdat_final_labels_val <- wdat_final_labels_t[-test_val_cols]
 #4 f. Compute magnitude of the weight vector every 30 steps.
 #4 g. Compute accuracy on the test set.
 
-a_val <- as.vector(0)
+a_val <- as.vector(c(0,0,0,0,0,0))
 lambda_val <- 10
 lambda <- 10
 b_val <- as.vector(0)
 n_val <- 0
-a_val_low <- as.vector(0)
+a_val_low <- as.vector(c(0,0,0,0,0,0))
 train_eval_df <- data.frame()
 
 wdat_final_features_val_matrix <- as.matrix(wdat_final_features_val)
@@ -53,14 +53,20 @@ for (m in 1:5) {
   
   lambda_val <- (lambda_val/10)
   print(lambda_val)
+  
+  a_val_low <- a_val
+  a_val <- as.vector(c(0,0,0,0,0,0))
+  b_val <- as.vector(0)
+  n_val <- 0
+  
 for (l in 1:nrow(wdat_final_features_val)) {
   
-  n_val = 1/(0.01 * 50 + 50)
+  n_val = 1/(0.01 * 100 + 50)
   
   x_val <- as.vector(wdat_final_features_val_matrix[l,])
   y_val <- wdat_final_labels_val_matrix[l,1]
   
-  condition_val <- (y_val * (sum(t(a_val),x_val) + b_val))
+  condition_val <- (y_val * (sum(t(a_val) * x_val) + b_val))
   
   if (condition_val >= 1) {
     a_val = a_val - n_val * (lambda_val * a_val)
@@ -71,18 +77,19 @@ for (l in 1:nrow(wdat_final_features_val)) {
   
  }
 
-  if (sum(t(a_val),a_val) < sum(t(a_val_low),a_val_low)) {
+  if (sum(t(a_val) * a_val) > sum(t(a_val_low) * a_val_low)) {
     a_val_low <- a_val
     lambda <- lambda_val
   }
 }
-  
+
 #for (z in 1:5) {
 
-a <- as.vector(0)
-lambda <- lambda_val
+a <- as.vector(c(0,0,0,0,0,0))
+lambda <- lambda_val #or try different regularizer values 1, 1e-1, 1e-2, 1e-3, 1e-4 etc
 b <- as.vector(0)
 n <- 0
+
 sumofright <- as.numeric(0)
 sumofwrong <- as.numeric(0)
 sumofright_eval <- as.numeric(0)
@@ -110,7 +117,7 @@ for (i in 1:50) {
     
     #Condition formulation : y(t(a).x + b)
     
-    condition <- (y * (sum(t(a),x) + b))
+    condition <- (y * (sum(t(a) * x) + b))
     
     #Setting up the Gradient
     
@@ -126,7 +133,7 @@ for (i in 1:50) {
        for (p in 1:nrow(wdat_final_features_eval_matrix)) {
          
          xi_eval <- as.vector(wdat_final_features_eval_matrix[p,])
-         predict_label_eval = sign(sum(t(a),xi_eval) + b)
+         predict_label_eval = sign(sum(t(a) * xi_eval) + b)
          if (predict_label_eval == wdat_final_labels_eval_matrix[p,1]) {
            sumofright_eval = sumofright_eval + 1
          } else {
@@ -136,6 +143,7 @@ for (i in 1:50) {
        }
        accuracy_eval <- sumofright_eval/(sumofright_eval + sumofwrong_eval)
        magnitude_of_a <- sqrt(a[1]^2+a[2]^2+a[3]^2+a[4]^2+a[5]^2+a[6]^2)
+       #magnitude_of_a <- sqrt(sum(t(a) * a))
        train_eval_df <- rbind.data.frame(train_eval_df,c(i,lambda,accuracy_eval,magnitude_of_a),stringsAsFactors = FALSE)
     } 
   
@@ -147,7 +155,7 @@ colnames(train_eval_df) <- c("epoch","regularizer","accuracy","magnitude")
 
 for (k in 1:nrow(wdat_final_features_test)) {
   xi <- as.vector(wdat_final_features_test_matrix[k,])
-  predict_label = sign(sum(t(a),xi) + b)
+  predict_label = sign(sum(t(a) * xi) + b)
   if (predict_label == wdat_final_labels_test_matrix[k,1]) {
     sumofright = sumofright + 1
   } else {
@@ -163,12 +171,12 @@ print(accuracy)
 
 #5. Plot the magnitude vector every 30 steps in each epoch.
 
-plot(train_eval_df$epoch[train_eval_df$regularizer==0.0001],train_eval_df$magnitude[train_eval_df$regularizer==0.0001],ylim=c(0,55),type = "l",col="red",xlab = "Epoch",ylab = "Magnitude")
-lines(train_eval_df$epoch[train_eval_df$regularizer==0.001],train_eval_df$magnitude[train_eval_df$regularizer==0.001],ylim=c(0,55),col="blue")
-lines(train_eval_df$epoch[train_eval_df$regularizer==0.01],train_eval_df$magnitude[train_eval_df$regularizer==0.01],ylim=c(0,55),col="dark green")
-lines(train_eval_df$epoch[train_eval_df$regularizer==0.1],train_eval_df$magintude[train_eval_df$regularizer==0.1],ylim=c(0,55),col="orange")
-lines(train_eval_df$epoch[train_eval_df$regularizer==1],train_eval_df$magnitude[train_eval_df$regularizer==1],ylim=c(0,55),col="cyan")
-legend("topleft","x,y",c("1e-4","1e-3","1e-2","1e-1","1"),lty=c(1,1),lwd=c(1.5,1.5,1.5,1.5,1.5),col=c("red","blue","dark green","orange","cyan"))
+plot(train_eval_df$epoch[train_eval_df$regularizer==0.0001],train_eval_df$magnitude[train_eval_df$regularizer==0.0001],ylim=c(0,3),type = "l",col="red",xlab = "Epoch",ylab = "Magnitude")
+lines(train_eval_df$epoch[train_eval_df$regularizer==0.001],train_eval_df$magnitude[train_eval_df$regularizer==0.001],ylim=c(0,3),col="blue")
+lines(train_eval_df$epoch[train_eval_df$regularizer==0.01],train_eval_df$magnitude[train_eval_df$regularizer==0.01],ylim=c(0,3),col="dark green")
+lines(train_eval_df$epoch[train_eval_df$regularizer==0.1],train_eval_df$magnitude[train_eval_df$regularizer==0.1],ylim=c(0,3),col="orange")
+lines(train_eval_df$epoch[train_eval_df$regularizer==1],train_eval_df$magnitude[train_eval_df$regularizer==1],ylim=c(0,3),col="cyan")
+legend("topleft","x,y",c("1e-4","1e-3","1e-2","1e-1","1"),lty=c(1,1),lwd=c(1.5,1.5,1.5,1.5,1.5),cex=0.75,col=c("red","blue","dark green","orange","cyan"))
 
 #6. Plot the accuracy computed on evaluation set every 30 steps in each epoch.
 
@@ -177,6 +185,6 @@ lines(train_eval_df$epoch[train_eval_df$regularizer==0.001],train_eval_df$accura
 lines(train_eval_df$epoch[train_eval_df$regularizer==0.01],train_eval_df$accuracy[train_eval_df$regularizer==0.01],ylim=c(0,1),col="dark green")
 lines(train_eval_df$epoch[train_eval_df$regularizer==0.1],train_eval_df$accuracy[train_eval_df$regularizer==0.1],ylim=c(0,1),col="orange")
 lines(train_eval_df$epoch[train_eval_df$regularizer==1],train_eval_df$accuracy[train_eval_df$regularizer==1],ylim=c(0,1),col="cyan")
-legend("bottomright","x,y",c("1e-4","1e-3","1e-2","1e-1","1"),lty=c(1,1),lwd=c(1.5,1.5,1.5,1.5,1.5),col=c("red","blue","dark green","orange","cyan"))
+legend("bottomright","x,y",c("1e-4","1e-3","1e-2","1e-1","1"),lty=c(1,1),lwd=c(1.5,1.5,1.5,1.5,1.5),cex=0.75,col=c("red","blue","dark green","orange","cyan"))
 
 
